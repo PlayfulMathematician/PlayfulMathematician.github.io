5bfc5c202fc32373e428cd348bfff3ccc4ad7355
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-11-20 10:28:59 -0700

    CHNAGE

--- home.js
+++ home.js
@@ -1,18 +1,19 @@
 
-var pages = ['coinspt1']
-
+var pages = [['coinspt1', 'A Very Specific Puzzle on Coin Flipping Part 1']];
+pages.push(["coinspt2", "A Very Specific Puzzle on Coin Flipping Part 2"])
 function main()
 {
     pageLinks = []
     for(var i = 0; i < pages.length; i++) {
-        var page = pages[i]
-        var pageLink = "https://playfulmathematician.github.io/" + page + ".html"
+        var page = pages[i][0]
+        var pageLink = "https://playfulmathematician.com/" + page + ".html"
         pageLinks.push(pageLink)
     }
-    $("body").append("<ul id='pageLinks'></ul>")
+    $("body").append("<div id='pageLinks'></div>")
     for(var i = 0; i < pageLinks.length; i++) {
         var pageLink = pageLinks[i]
-        $("#pageLinks").append("<li><a href='" + pageLink + "'>" + pages[i] + "</a></li>")
+        $("#pageLinks").append("<span class=\'link\'><a href='" + pageLink + "' class = \'newpage\'>" + pages[i][1] + "</a></span>")
+        $("#pageLinks").append("<div class = \"spacer-div-20px\"></div>")
     }
 
 
