3b09af703e9bb8b1516ee6b7def2c9ff38b3ef0d
PlayfulMathematician <73261021+PlayfulMathematician@users.noreply.github.com>
2022-11-19 19:14:34 -0700

    HI

--- home.js
+++ home.js
@@ -1,1 +1,20 @@
-// This code will add a div for each webpage listed in pages.txt
+
+var pages = ['coinspt1']
+
+function main()
+{
+    pageLinks = []
+    for(var i = 0; i < pages.length; i++) {
+        var page = pages[i]
+        var pageLink = "https://playfulmathematician.github.io/" + page + ".html"
+        pageLinks.push(pageLink)
+    }
+    $("body").append("<ul id='pageLinks'></ul>")
+    for(var i = 0; i < pageLinks.length; i++) {
+        var pageLink = pageLinks[i]
+        $("#pageLinks").append("<li><a href='" + pageLink + "'>" + pages[i] + "</a></li>")
+    }
+
+
+}
+$(document).ready(main)
