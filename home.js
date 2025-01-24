a49c6dabbeac5842e9c4839d977796d3c63ff1c8
undeterminstic <73261021+PlayfulMathematician@users.noreply.github.com>
2025-01-24 11:34:45 -0700

    Update home.js

--- home.js
+++ home.js
@@ -1,7 +1,7 @@
 
 var pages = [];
 // pages.push(["coinspt2", "A Very Specific Puzzle on Coin Flipping Part 2"])
-pages.push(["zomebuilds", "My Zome Builds"])
+pages.push(["platoniccompounds", "Platonic Compounds"])
 pages.push(['coinspt1', 'A Very Specific Puzzle on Coin Flipping Part 1'])
 
 function main()
